e0d8dd876efea75549ad3a05c48a627c32720cb7
Will Nissler <67075726+wOkEaF101@users.noreply.github.com>
2022-07-28 12:28:01 -0600

    Cleaned for brevity

--- loggerhead/go.mod
+++ loggerhead/go.mod
@@ -4,6 +4,5 @@ go 1.18
 
 require (
 	github.com/bi-zone/etw v0.0.0-20210519083747-fe9042eb0ea8
-	github.com/mitchellh/mapstructure v1.5.0
 	golang.org/x/sys v0.0.0-20220727055044-e65921a090b8
 )
--- loggerhead/loggerhead.go
+++ loggerhead/loggerhead.go
@@ -3,8 +3,5 @@ package main
 import loggerhead "loggerhead/internal"
 
 func main() {
-	//loggerhead.ProcETW()
-	//loggerhead.TestETW()
-	//loggerhead.NetConnETW()
 	loggerhead.ETWSession("{22FB2CD6-0E7B-422B-A0C7-2FAD1FD0E716}")
 }
